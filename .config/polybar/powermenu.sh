cf8799fb2115f98a72b50b5509f73ccf1d34eede
lharck <LuzHarck@gmail.com>
2024-02-12 16:40:46 -0600

    added wallpaper and scripts

---
+++ .config/polybar/powermenu.sh
@@ -0,0 +1,8 @@
+rofi \
+  -show p \
+  -modi p:'rofi-power-menu --symbols-font "Symbols Nerd Font Mono"' \
+  -font "FiraCode Nerd Font 100log" \
+  -theme gruvbox-dark-hard \
+  -theme-str 'window {width: 10em;} listview {lines: 6;}' \
+  -location 3 \
+  -yoffset 35  
